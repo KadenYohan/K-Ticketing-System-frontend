18c66173367605c762e0343a2fbda6ab47bd083e
kennethlance2005 <kllibardaloza@mymail.mapua.edu.ph>
2026-05-29 00:46:28 +0800

    Deleted

--- src/api/index.js/index.js
+++
@@ -1,6 +0,0 @@
-import { CONFIG } from '../config';
-import * as realApi from './real';
-import * as mockApi from '../mock/api';
-
-// Uniform wrapper executing structural code switching
-export const API = CONFIG.USE_MOCK ? mockApi : realApi;
--- src/config/index.js/index.js
+++
@@ -1,4 +0,0 @@
-export const CONFIG = {
-  USE_MOCK: true, 
-  API_BASE_URL: 'http://localhost:3000',
-};
